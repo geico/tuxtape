e5c0fb491777c2ff851f15cf79a56e2c18b051e2
Grayson Guarino <graysonguarino@gmail.com>
2025-03-21 14:54:03 +0000

    Run all pending migrations at startup

--- tuxtape-server/src/main.rs
+++ tuxtape-server/src/main.rs
@@ -1,17 +1,26 @@
 use clap::Parser;
 use cli::Cli;
-use color_eyre::Result;
-use database::connection;
+use color_eyre::{Result, eyre::eyre};
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 
 mod cli;
 mod database;
+
+// Embed all migrations into the binary
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
 
-    // Attempt to connect to database and exit early and loudly if failed.
-    connection::establish_connection(args.db_backend, &args.db_url)?;
+    {
+        // Attempt to connect to database and exit early and loudly if failed.
+        let mut conn = database::connection::establish_connection(args.db_backend, &args.db_url)?;
+
+        // Run any pending database migrations before proceeding.
+        conn.run_pending_migrations(MIGRATIONS)
+            .map_err(|e| eyre!(e))?;
+    }
 
     Ok(())
 }
