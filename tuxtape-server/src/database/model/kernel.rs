234ba7d031a20c91d7060a24be2abc8e0222d91d
Grayson Guarino <graysonguarino@gmail.com>
2025-03-21 14:22:29 +0000

    Add complete models

--- tuxtape-server/src/database/model/kernel.rs
+++ tuxtape-server/src/database/model/kernel.rs
@@ -1,1 +1,88 @@
-// TODO
+use crate::database::schema;
+use diesel::prelude::*;
+use diesel_attributes::*;
+
+#[diesel_model_fetch(schema::kernel_file)]
+#[diesel(belongs_to(KernelRelease))]
+pub struct KernelFile {
+    pub id: i32,
+    pub kernel_release_id: i32,
+    pub file_path: String,
+}
+
+#[diesel_model_insert(schema::kernel_file)]
+pub struct InsertKernelFile<'a> {
+    pub kernel_release_id: i32,
+    pub file_path: &'a str,
+}
+
+#[diesel_model_update(schema::kernel_file)]
+pub struct UpdateKernelFile<'a> {
+    pub kernel_release_id: Option<i32>,
+    pub file_path: Option<&'a str>,
+}
+
+#[diesel_model_fetch(schema::kernel_release)]
+#[diesel(belongs_to(MainlineKernelRelease))]
+pub struct KernelRelease {
+    pub id: i32,
+    pub mainline_kernel_release_id: i32,
+    pub version_local: Option<String>,
+}
+
+#[diesel_model_insert(schema::kernel_release)]
+pub struct InsertKernelRelease<'a> {
+    pub mainline_kernel_release_id: i32,
+    pub version_local: Option<&'a str>,
+}
+
+#[diesel_model_update(schema::kernel_release)]
+pub struct UpdateKernelRelease<'a> {
+    pub mainline_kernel_release_id: Option<i32>,
+    pub version_local: Option<Option<&'a str>>,
+}
+
+#[diesel_model_fetch(schema::mainline_kernel_release)]
+pub struct MainlineKernelRelease {
+    pub id: i32,
+    pub version_major: i32,
+    pub version_minor: i32,
+    pub version_patch: i32,
+    pub version_extra: String,
+}
+
+#[diesel_model_insert(schema::mainline_kernel_release)]
+pub struct InsertMainlineKernelRelease<'a> {
+    pub version_major: i32,
+    pub version_minor: i32,
+    pub version_patch: i32,
+    pub version_extra: &'a str,
+}
+
+#[diesel_model_update(schema::mainline_kernel_release)]
+pub struct UpdateMainlineKernelRelease<'a> {
+    pub version_major: Option<i32>,
+    pub version_minor: Option<i32>,
+    pub version_patch: Option<i32>,
+    pub version_extra: Option<&'a str>,
+}
+
+#[diesel_model_fetch(schema::kernel_source)]
+#[diesel(belongs_to(KernelRelease))]
+pub struct KernelSource {
+    pub id: i32,
+    pub kernel_release_id: i32,
+    pub url: String,
+}
+
+#[diesel_model_insert(schema::kernel_source)]
+pub struct InsertKernelSource<'a> {
+    pub kernel_release_id: i32,
+    pub url: &'a str,
+}
+
+#[diesel_model_update(schema::kernel_source)]
+pub struct UpdateKernelSource<'a> {
+    pub kernel_release_id: Option<i32>,
+    pub url: Option<&'a str>,
+}
--- tuxtape-server/src/database/model/meta.rs
+++ tuxtape-server/src/database/model/meta.rs
@@ -1,10 +1,22 @@
 use crate::database::schema;
 use diesel::prelude::*;
+use diesel_attributes::*;
 
+#[diesel_model_fetch(schema::meta)]
 pub struct Meta {
-    // TODO
+    pub id: i32,
+    pub based_on_vulns_commit: String,
+    pub last_run_unix_time: i32,
 }
 
-pub struct NewMeta {
-    // TODO
+#[diesel_model_insert(schema::meta)]
+pub struct InsertMeta<'a> {
+    pub based_on_vulns_commit: &'a str,
+    pub last_run_unix_time: i32,
+}
+
+#[diesel_model_update(schema::meta)]
+pub struct UpdateMeta<'a> {
+    pub based_on_vulns_commit: Option<&'a str>,
+    pub last_run_unix_time: Option<i32>,
 }
