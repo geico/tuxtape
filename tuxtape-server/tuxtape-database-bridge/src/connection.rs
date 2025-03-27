378d66e26f7614d9447500cd83c3b5e0feb10577
Grayson Guarino <graysonguarino@gmail.com>
2025-03-27 20:54:10 +0000

    Add DatabaseConnectionDetails definition to bridge

--- tuxtape-server/tuxtape-database-bridge/src/connection.rs
+++ tuxtape-server/tuxtape-database-bridge/src/connection.rs
@@ -15,18 +15,32 @@ pub enum AnyConnection {
     Sqlite(diesel::SqliteConnection),
 }
 
+/// The arguments required to form a database connection.
+pub struct DatabaseConnectionDetails {
+    pub backend: DatabaseBackend,
+    pub db_url: String,
+}
+
+impl DatabaseConnectionDetails {
+    pub fn new(backend: DatabaseBackend, db_url: &str) -> Self {
+        DatabaseConnectionDetails {
+            backend,
+            db_url: db_url.to_string(),
+        }
+    }
+}
+
 /// Establish a connection to a database backend.
 pub fn establish_connection(
-    backend: DatabaseBackend,
-    database_url: &str,
+    conn_details: &DatabaseConnectionDetails,
 ) -> ConnectionResult<AnyConnection> {
-    match backend {
+    match conn_details.backend {
         DatabaseBackend::Sqlite => {
-            let sqlite_connection = SqliteConnection::establish(database_url)?;
+            let sqlite_connection = SqliteConnection::establish(&conn_details.db_url)?;
             Ok(AnyConnection::Sqlite(sqlite_connection))
         }
         DatabaseBackend::Pg => {
-            let pg_connection = PgConnection::establish(database_url)?;
+            let pg_connection = PgConnection::establish(&conn_details.db_url)?;
             Ok(AnyConnection::Pg(pg_connection))
         }
     }
