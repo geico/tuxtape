f61de0a6a5ece78e830393517cb8c39d6761ec80
Grayson Guarino <graysonguarino@gmail.com>
2025-04-25 14:18:41 -0400

    Begin implementing grpc request handlers

--- tuxtape-server/src/tuxtape_database_bridge/grpc.rs
+++ tuxtape-server/src/tuxtape_database_bridge/grpc.rs
@@ -1,24 +1,50 @@
+use crate::{
+    error::{DatabaseBridgeError, Result},
+    handlers,
+};
 use proto::tuxtape::server::database::v1::*;
 use sqlx::PgConnection;
-use tonic::{Response, Status};
+use tonic::Response;
 
-pub fn get_vulnerabilities(
+pub async fn get_vulnerabilities(
     conn: &mut PgConnection,
     request: &GetVulnerabilitiesRequest,
-) -> Result<Response<GetVulnerabilitiesResponse>, Status> {
-    todo!();
+) -> Result<Response<GetVulnerabilitiesResponse>> {
+    use proto::tuxtape::server::database::v1::get_vulnerabilities_request::GetBy;
+    // TODO - implement excludes
+
+    let get_by = if let Some(get_by) = &request.get_by {
+        get_by
+    } else {
+        return Err(DatabaseBridgeError::proto_missing_field("get_by"));
+    };
+
+    let vulnerabilities = match get_by {
+        GetBy::All(_) => handlers::vulnerability::get_all_vulnerabilities(conn).await,
+        GetBy::KernelRelease(by_kernel_release) => {
+            handlers::vulnerability::get_all_vulnerabilities_affecting_kernel_releases(
+                &by_kernel_release.kernel_releases,
+                conn,
+            )
+            .await
+        }
+    }?;
+
+    Ok(Response::new(GetVulnerabilitiesResponse {
+        vulnerabilities,
+    }))
 }
 
-pub fn get_kernel_source(
+pub async fn get_kernel_source(
     conn: &mut PgConnection,
     request: &GetKernelSourceRequest,
-) -> Result<Response<GetKernelSourceResponse>, Status> {
+) -> Result<Response<GetKernelSourceResponse>> {
     todo!();
 }
 
-pub fn create_kernel(
+pub async fn create_kernel(
     conn: &mut PgConnection,
     request: &CreateKernelRequest,
-) -> Result<Response<CreateKernelResponse>, Status> {
+) -> Result<Response<CreateKernelResponse>> {
     todo!();
 }
