a1aa15f710aac412536961a3b6c3e8f53d2b12e5
Grayson Guarino <graysonguarino@gmail.com>
2025-05-19 11:52:37 -0400

    [Server] Call bridge methods from tuxtape-server service handlers

--- tuxtape-server/src/grpc/service/database.rs
+++ tuxtape-server/src/grpc/service/database.rs
@@ -1,7 +1,8 @@
-use proto::tuxtape::server::database::v1::database_service_server::DatabaseService;
-use proto::tuxtape::server::database::v1::*;
+use super::get_connection;
+use proto::tuxtape::server::database::v1::{database_service_server::DatabaseService, *};
 use sqlx::PgPool;
 use tonic::{Request, Response, Status};
+use tuxtape_database_bridge::grpc as db_bridge;
 
 pub struct DatabaseServiceState {
     db_pool: PgPool,
@@ -19,36 +20,53 @@ impl DatabaseServiceState {
 impl DatabaseService for DatabaseServiceState {
     async fn get_vulnerabilities(
         &self,
-        request: Request<GetVulnerabilitiesRequest>,
+        _request: Request<GetVulnerabilitiesRequest>,
     ) -> Result<Response<GetVulnerabilitiesResponse>, Status> {
-        todo!()
+        let mut conn = get_connection(&self.db_pool).await?;
+        let response = db_bridge::get_vulnerabilities(&mut conn).await?;
+
+        Ok(response)
+    }
+
+    async fn get_vulnerability_instances(
+        &self,
+        request: Request<GetVulnerabilityInstancesRequest>,
+    ) -> Result<Response<GetVulnerabilityInstancesResponse>, Status> {
+        let mut conn = get_connection(&self.db_pool).await?;
+        let response =
+            db_bridge::get_vulnerability_instances(&request.into_inner(), &mut conn).await?;
+
+        Ok(response)
     }
 
     async fn get_kernel_source(
         &self,
         request: Request<GetKernelSourceRequest>,
     ) -> Result<Response<GetKernelSourceResponse>, Status> {
-        todo!()
+        let mut conn = get_connection(&self.db_pool).await?;
+        let response = db_bridge::get_kernel_source(&request.into_inner(), &mut conn).await?;
+
+        Ok(response)
     }
 
     async fn get_affected_kernel_releases(
         &self,
         request: Request<GetAffectedKernelReleasesRequest>,
     ) -> Result<Response<GetAffectedKernelReleasesResponse>, Status> {
-        todo!()
-    }
+        let mut conn = get_connection(&self.db_pool).await?;
+        let response =
+            db_bridge::get_affected_kernel_releases(&request.into_inner(), &mut conn).await?;
 
-    async fn get_affected_kernel_files(
-        &self,
-        request: Request<GetAffectedKernelFilesRequest>,
-    ) -> Result<Response<GetAffectedKernelFilesResponse>, Status> {
-        todo!()
+        Ok(response)
     }
 
     async fn create_kernel(
         &self,
         request: Request<CreateKernelRequest>,
     ) -> Result<Response<CreateKernelResponse>, Status> {
-        todo!()
+        let mut conn = get_connection(&self.db_pool).await?;
+        let response = db_bridge::create_kernel(&request.into_inner(), &mut conn).await?;
+
+        Ok(response)
     }
 }
